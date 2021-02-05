01a6f4cd3e4cd5c4134da4fc6026ce63e7a0974e
Christopher Hill <chill@redlineadvantage.com>
2021-02-05 14:00:50 -0500

    Image dedup update

--- dedup.js
+++ dedup.js
@@ -25,6 +25,10 @@ async function getJimpImage(url){
 	return await Jimp.read({ url });
 }
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function process(file,ratio=0.3,quality=100,base64=true){
 	const jimpImg = await (typeof file === "string" ? 
 					getJimpImage(file) : 
@@ -33,7 +37,7 @@ async function process(file,ratio=0.3,quality=100,base64=true){
 	const x = parseInt(640 * ratio);
 	const y = parseInt(480 * ratio);
 	const qcrop = await resized.quality(quality);
-	const cropped = await qcrop.crop(x, y, 150, 100).rgba(false).greyscale();
+	const cropped = await qcrop.crop(x, y, 200, 200).rgba(false).greyscale();
 	//console.log("JIMP IMG",cropped);
 	return await (base64 ? cropped.getBase64Async("image/jpeg") : cropped.getBufferAsync("image/jpeg"));
 }
@@ -68,27 +72,43 @@ let net;
 	// Load the model.
 	net = await mobilenet.load();
 	console.log('Successfully loaded model');
+
 	const classes = ["correct","incorrect"];
 
-	const correct = document.getElementById("correct");
-	const incorrect = document.getElementById("incorrect");
+	const train = document.getElementById("correct");
 	const predictButton = document.getElementById("predict");
 	const predictOutput = document.getElementById("prediction");
 
-	const qualities = [100,95,94,93,92,91,90,80,81,82,70,71,72,50,55,40,30,10,11,6,5,4,3,2];
-	//const ratios = [0.3,0.4,0.5,0.2];
-	const ratios = [0.3];
-	correct.onclick = async event => {
+	const qualities = [100,50,94,93,92,91,90,80,81,82,70];
+	const ratios = [0.2,0.3];
+
+	/**
+	 * Train the model 
+	 * 
+	 * @param {*} event 
+	 */
+	train.onclick = async event => {
+
+		train.disabled = true;
+		const loadingSpinner = document.getElementById("training-loading");
+		const trainingText = document.getElementById("train-text");
+		trainingText.innerText = "Training correct samples...";
+		loadingSpinner.classList.remove("d-none");
+		const rawPhoto = originalPreview.src;
+
 		// Get the intermediate activation of MobileNet 'conv_preds' and pass that
 		// to the KNN classifier.
-		await Promise.all(qualities.map(async qu => {
-			var item = ratios[Math.floor(Math.random() * ratios.length)];
-			const processed = await process(originalPreview.src,item,qu);
+		await Promise.all(qualities.map(async qualityPercentage => {
+
+			const ratio = ratios[ Math.floor(Math.random() * ratios.length) ];
+			const processed = await process(rawPhoto, ratio, qualityPercentage);
+
 			previewContainer.src = processed;
 			const img = tf.browser.fromPixels(previewContainer);
 			const activation = net.infer(img, true);
-			console.log(`Trained correct...${qu}`);
-			// Pass the intermediate activation to the classifier.
+
+			console.log(`Trained correct...${qualityPercentage}`);
+
 			classifier.addExample(activation, 0);
 		}));
 
@@ -113,11 +133,12 @@ let net;
 			"inc_8",
 			"inc_9"
 		];
+		trainingText.innerText = "Training incorrect samples...";
 
 		await Promise.all(incorrect.map(async inc => {
-			var item = ratios[Math.floor(Math.random() * ratios.length)];
+			const ratio = ratios[Math.floor(Math.random() * ratios.length)];
 			const incorrect = document.getElementById(inc);
-			const processed = await process(incorrect.src,item);
+			const processed = await process(incorrect.src,ratio);
 			previewContainer.src = processed;
 			const img = tf.browser.fromPixels(previewContainer);
 			const activation = net.infer(img, true);
@@ -125,23 +146,13 @@ let net;
 			// Pass the intermediate activation to the classifier.
 			classifier.addExample(activation, 1);
 		}));
-		
-	}
 
-	/*
-	incorrect.onclick = event => {
-		// Get the intermediate activation of MobileNet 'conv_preds' and pass that
-		// to the KNN classifier.
-		const img = tf.browser.fromPixels(originalPreview);
-		const activation = net.infer(img, true);
-		console.log("Trained incorrect...");
-		// Pass the intermediate activation to the classifier.
-		classifier.addExample(activation, 1);
+		trainingText.innerText = "Train";
+		loadingSpinner.classList.add("d-none");
 	}
-	*/
 
 	predictButton.onclick = async event => {
-		const preview = await process(challengePreview.src,0.3);
+		const preview = await process(challengePreview.src,0.2);
 		challengePreview.src = preview;
 		// Get the activation from mobilenet from the webcam.
 		const activation = net.infer(challengePreview, 'conv_preds');
@@ -149,7 +160,7 @@ let net;
 		// Get the most likely class and confidence from the classifier module.
 		const result = await classifier.predictClass(activation);
 		const confidence = parseInt((result.confidences[result.label]*100).toFixed(2));
-		console.log(confidence,result.label,typeof confidence);
+		console.log({ result });
 
 		const message = confidence > 95 && result.label == 0   ? 
 						"This is the correct image (duplicate)" :
