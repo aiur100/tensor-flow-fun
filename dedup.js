85f367e13792b6cb87c63c6d6c4a34a7c64440ec
Christopher <chrishill9@gmail.com>
2021-02-06 13:27:04 -0500

    Finally got this thing working

--- dedup.js
+++ dedup.js
@@ -67,6 +67,15 @@ challenge.onchange = event => {
 const classifier = knnClassifier.create();
 let net;
 
+function getByteNumbers(base64string){
+	const b = atob(base64string)
+	let byteNumbers = new Array(b.length);
+	for (let i = 0; i < b.length; i++) {
+		byteNumbers[i] = b.charCodeAt(i);
+	}
+	return byteNumbers;
+}
+
 (async function(){
 
 	// Load the model.
@@ -79,8 +88,19 @@ let net;
 	const predictButton = document.getElementById("predict");
 	const predictOutput = document.getElementById("prediction");
 
-	const qualities = [100,50,40,30,10,5,6,3,2,1];
-	const ratios = [0.3];
+	const qualities = [100,50,40,2];
+	const ratios = [0.3,0.4];
+
+	function createImage(base64str){
+		return new Promise((resolve, reject) => {
+			const im = new Image();
+			im.crossOrigin = 'anonymous';
+			im.src = base64str;
+			im.onload = () => {
+				resolve(im)
+			};
+		})
+	}
 
 	/**
 	 * Train the model 
@@ -96,41 +116,59 @@ let net;
 		loadingSpinner.classList.remove("d-none");
 		const rawPhoto = originalPreview.src;
 
-		// Get the intermediate activation of MobileNet 'conv_preds' and pass that
-		// to the KNN classifier.
-		await Promise.all(qualities.map(async qualityPercentage => {
-
+		for(let i = 0; i < qualities.length; i++){
+			const qualityPercentage = qualities[i];
 			const ratio = ratios[ Math.floor(Math.random() * ratios.length) ];
+
 			const processed = await process(rawPhoto, ratio, qualityPercentage);
 
-			previewContainer.src = processed;
-			const img = tf.browser.fromPixels(processed);
+			const imgEl = await createImage(processed);
+
+			document.getElementById("leftContainer").appendChild(imgEl);
+			
+			const img = tf.browser.fromPixels(imgEl);
 			const activation = net.infer(img, true);
+			activation.print();
 
 			console.log(`Trained correct...${qualityPercentage}`);
 
 			classifier.addExample(activation, 0);
-		}));
+			await sleep(1000);
+		}
+
+		
+		trainingText.innerText = "Training incorrect samples...";
 
 		const incorrect = [
 			"inc_1",
 			"inc_2",
 			"inc_3",
-			"inc_4"
+			"inc_4",
+			"inc_5",
+			"inc_6",
+			"inc_7",
+			"inc_8"
 		];
-		trainingText.innerText = "Training incorrect samples...";
 
-		await Promise.all(incorrect.map(async inc => {
-			const ratio = ratios[Math.floor(Math.random() * ratios.length)];
-			const incorrect = document.getElementById(inc);
-			const processed = await process(incorrect.src,ratio);
-			previewContainer.src = processed;
-			const img = tf.browser.fromPixels(previewContainer);
+		for(let x = 0; x < incorrect.length; x++){
+			const inc = incorrect[x];
+			const ratio = ratios[ Math.floor(Math.random() * ratios.length) ];
+			const incElm = document.getElementById(inc);
+			const processed = await process(incElm.src,ratio);
+
+			const imgEl = await createImage(processed);
+			//previewContainer.src = processed;
+			console.log(previewContainer.src);
+			document.getElementById("leftContainer").appendChild(imgEl);
+
+			const img = tf.browser.fromPixels(imgEl);
 			const activation = net.infer(img, true);
+			activation.print();			
 			console.log(`Trained incorrect...${inc}`);
 			// Pass the intermediate activation to the classifier.
 			classifier.addExample(activation, 1);
-		}));
+			await sleep(1000);
+		}
 
 		trainingText.innerText = "Train";
 		loadingSpinner.classList.add("d-none");
@@ -141,11 +179,13 @@ let net;
 	predictButton.onclick = async event => {
 		const preview = await process(challengePreview.src,0.3);
 		challengePreview.src = preview;
-		// Get the activation from mobilenet from the webcam.
-		const activation = net.infer(challengePreview, 'conv_preds');
+		const imgEl = await createImage(preview);
+
+		const img = tf.browser.fromPixels(imgEl);
+		const activation = net.infer(img, 'conv_preds');
 
 		// Get the most likely class and confidence from the classifier module.
-		const result = await classifier.predictClass(activation,10);
+		const result = await classifier.predictClass(activation);
 		const confidence = parseInt((result.confidences[result.label]*100).toFixed(2));
 		console.log({ result });
 
